541261489f186ccf874d1d9902df03b56833638b
GHLisz <leeshzh@outlook.com>
2017-12-08 23:18:46 +0800

    findPeak

---
+++ lintcode/75-find-peak-element.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param: A: An integers array.
+    @return: return any of peek positions.
+    """
+
+    def findPeak(self, A):
+        # write your code here
+        def binary(nums, start, end):
+            if start == end:
+                return start
+            if end - start == 1:
+                return start if nums[start] > nums[end] else end
+            mid = (start + end) // 2
+            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                return mid
+            elif nums[mid] < nums[mid - 1]:
+                return binary(nums, start, mid - 1)
+            else:
+                return binary(nums, mid + 1, end)
+
+        return binary(A, 0, len(A) - 1)
+
