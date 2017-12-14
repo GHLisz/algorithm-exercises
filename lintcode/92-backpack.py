67926fb4ab5926cb3a99e7d4e329273ac5355d30
GHLisz <leeshzh@outlook.com>
2017-12-14 22:11:55 +0800

    backPack

---
+++ lintcode/92-backpack.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param m: An integer m denotes the size of a backpack
+    # @param A: Given n items with size A[i]
+    # @return: The maximum size
+    def backPack(self, m, A):
+        # write your code here
+        dp = [0 for _ in range(m + 1)]
+        dp[0] = 1
+        ans = 0
+        for item in A:
+            for i in range(m, -1, -1):
+                if i - item >= 0 and dp[i - item] > 0:
+                    ans = max(ans, i)
+                    dp[i] = 1
+        return ans
