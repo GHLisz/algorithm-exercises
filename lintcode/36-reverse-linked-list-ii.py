70d48bea511e738d5aa51052e256f963a3df6736
GHLisz <leeshzh@outlook.com>
2017-12-16 22:54:22 +0800

    reverseBetween

--- lintcode/36-reverse-linked-list-ii.py
+++ lintcode/36-reverse-linked-list-ii.py
@@ -20,22 +20,23 @@ class Solution:
     def reverseBetween(self, head, m, n):
         # write your code here
         dummy = ListNode(0, head)
-        cur = dummy
-        for i in range(1, m):
-            cur = cur.next
+        pre = dummy  # make a pointer pre as a marker for the node before reversing
 
-        pre_m_node = cur
-        m_node = cur.next
-        n_node = m_node
-        post_n_node = m_node.next
+        for i in range(1, m):
+            pre = pre.next
+
+        start = pre.next  # a pointer to the beginning of a sub-list that will be reversed
+        then = start.next  # a pointer to a node that will be reversed
+
+        # 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
 
         for i in range(m, n):
-            temp = post_n_node.next
-            post_n_node.next = n_node
-            n_node = post_n_node
-            post_n_node = temp
+            start.next = then.next
+            then.next = pre.next
+            pre.next = then
+            then = start.next
 
-        m_node.next = post_n_node
-        pre_m_node.next = n_node
+        # first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        # second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
 
         return dummy.next
