db6210a4bd8429e1fc204c0cf00340e5e1de4389
GHLisz <leeshzh@outlook.com>
2018-01-09 21:34:58 +0800

    Segment Tree Build

---
+++ lintcode/201-segment-tree-build.py
@@ -0,0 +1,21 @@
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end = start, end
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    # @param start, end: Denote an segment / interval
+    # @return: The root of Segment Tree
+    def build(self, start, end):
+        # write your code here
+        if start > end:
+            return None
+        if start == end:
+            return SegmentTreeNode(start, end)
+        root = SegmentTreeNode(start, end)
+        root.left = self.build(start, (start+end)/2)
+        root.right = self.build((start+end)/2+1, end)
+        return root
