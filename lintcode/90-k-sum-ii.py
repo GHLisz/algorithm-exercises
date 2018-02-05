e1cf8ff124c28d5a53928e6f9b45c7f16928fbd5
GHLisz <leeshzh@outlook.com>
2018-02-05 21:33:57 +0800

    kSumII

---
+++ lintcode/90-k-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param: A: an integer array
+    @param: k: a postive integer <= length(A)
+    @param: targer: an integer
+    @return: A list of lists of integer
+    """
+
+    def kSumII(self, A, k, target):
+        # write your code here
+        def dfs(A, k, target, index, tmp, res):
+            if target == 0 and k == 0:
+                res.append(tmp)
+                return
+            if len(A) == index or target < 0 or k < 0:
+                return
+            dfs(A, k, target, index + 1, tmp, res)
+            tmp2 = [A[index]]
+            tmp2.extend(tmp)
+            dfs(A, k - 1, target - A[index], index + 1, tmp2, res)
+
+        res = []
+        dfs(A, k, target, 0, [], res)
+        return res
