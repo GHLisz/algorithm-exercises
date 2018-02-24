1cb4b88edda1bb326a8ff1d831cfd8fb7cf7cad6
GHLisz <leeshzh@outlook.com>
2018-02-24 22:16:01 +0800

    majorityNumberii

---
+++ lintcode/48-majority-number-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param: nums: A list of integers
+    @param: k: An integer
+    @return: The majority number
+    """
+    def majorityNumber(self, nums, k):
+        # write your code here
+        counts, max = {}, 0
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+            if counts[num] > max:
+                max = counts[num]
+                majority = num
+        return majority
