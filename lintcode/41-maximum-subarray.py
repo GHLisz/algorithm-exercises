ca253c2213a48912894e76d0c6f6f9efa28efc66
GHLisz <leeshzh@outlook.com>
2018-03-10 21:55:05 +0800

    maxSubArray

--- lintcode/41-maximum-subarray.py
+++ lintcode/41-maximum-subarray.py
@@ -3,7 +3,6 @@ class Solution:
     @param nums: A list of integers
     @return: An integer denote the sum of maximum subarray
     """
-
     def maxSubArray(self, nums):
         # write your code here
         if nums is None or len(nums) == 0:
@@ -13,7 +12,7 @@ class Solution:
 
         for num in nums:
             real_sum += num
-            max_sum = real_sum - min_sum if real_sum - min_sum > max_sum else max_sum
-            min_sum = real_sum if real_sum < min_sum else min_sum
+            max_sum = max(real_sum - min_sum, max_sum)
+            min_sum = min(real_sum, min_sum)
 
         return max_sum
