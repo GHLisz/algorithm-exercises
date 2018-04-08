3197f1cd39382e60efd6cc7655eff007ca7f7667
GHLisz <leeshzh@outlook.com>
2018-04-08 21:03:49 +0800

    binarySearch

--- lintcode/14-first-position-of-target.py
+++ lintcode/14-first-position-of-target.py
@@ -1,18 +1,24 @@
 class Solution:
-    # @param nums: The integer array
-    # @param target: Target number to find
-    # @return the first position of target in nums, position start from 0
+    """
+    @param nums: The integer array.
+    @param target: Target to find.
+    @return: The first position of target. Position starts from 0.
+    """
+
     def binarySearch(self, nums, target):
         # write your code here
         low, high = 0, len(nums) - 1
 
-        while low <= high:
+        while low + 1 < high:
             mid = (low + high) // 2
-            if nums[mid] == target and nums[mid - 1] != target:
-                return mid
-            elif nums[mid] >= target:
-                high = mid - 1
+            if target <= nums[mid]:
+                high = mid
             else:
                 low = mid + 1
 
-        return -1
+        if nums[low] == target:
+            return low
+        elif nums[high] == target:
+            return high
+        else:
+            return -1
--- lintcode/897-island-city.py
+++ lintcode/897-island-city.py
@@ -3,7 +3,6 @@ class Solution:
     @param grid: an integer matrix
     @return: an integer
     """
-
     def numIslandCities(self, grid):
         # Write your code here
         def dfs(grid, x, y):
@@ -12,9 +11,8 @@ class Solution:
             if grid[x][y] == 0:
                 return False
 
-            have_city = False
-            if grid[x][y] == 2:
-                have_city = True
+            have_city = True if grid[x][y] == 2 else False
+
             grid[x][y] = 0
             have_city = dfs(grid, x + 1, y) or have_city
             have_city = dfs(grid, x - 1, y) or have_city
@@ -22,13 +20,12 @@ class Solution:
             have_city = dfs(grid, x, y - 1) or have_city
             return have_city
 
-        if not grid or not grid[0]:
-            return 0
+        if not grid or not grid[0]: return 0
 
         m, n, cnt = len(grid[0]), len(grid), 0
+
         for i in range(n):
             for j in range(m):
-                if grid[i][j]:
-                    if dfs(grid, i, j):
-                        cnt += 1
+                if grid[i][j] and dfs(grid, i, j):
+                    cnt += 1
         return cnt
