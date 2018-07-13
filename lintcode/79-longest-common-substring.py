a9d042de938081dfe33aa413385a7ec03b416439
GHLisz <leeshzh@outlook.com>
2018-07-13 21:59:57 +0800

    maxDepth

--- lintcode/79-longest-common-substring.py
+++ lintcode/79-longest-common-substring.py
@@ -1,23 +1,17 @@
 class Solution:
     """
-    @param: A: A string
-    @param: B: A string
+    @param A: A string
+    @param B: A string
     @return: the length of the longest common substring.
     """
 
     def longestCommonSubstring(self, A, B):
         # write your code here
-        dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        a, b = len(A), len(B)
 
-        for i in range(1, len(A) + 1):
-            for j in range(1, len(B) + 1):
-                if A[i - 1] == B[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] = 0
+        dp = [[0] * (b + 1) for _ in range(a + 1)]
+        for i in range(1, a + 1):
+            for j in range(1, b + 1):
+                dp[i][j] = dp[i - 1][j - 1] + 1 if A[i - 1] == B[j - 1] else 0
 
-        ans = 0
-        for i in range(1, len(A) + 1):
-            for j in range(1, len(B) + 1):
-                ans = max(ans, dp[i][j])
-        return ans
+        return max((max(sub) for sub in dp), default=0)
