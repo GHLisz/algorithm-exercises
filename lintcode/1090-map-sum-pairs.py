d5be4a87fac301341234ddf3fd46013d185de6ad
GHLisz <leeshzh@outlook.com>
2018-08-25 21:31:58 +0800

    MapSum

---
+++ lintcode/1090-map-sum-pairs.py
@@ -0,0 +1,45 @@
+class MapSum:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.map = {}
+        self.root = TrieNode()
+
+    def insert(self, key, val):
+        """
+        :type key: str
+        :type val: int
+        :rtype: void
+        """
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+        cur = self.root
+        cur.score += delta
+        for char in key:
+            cur = cur.children.setdefault(char, TrieNode())
+            cur.score += delta
+
+    def sum(self, prefix):
+        """
+        :type prefix: str
+        :rtype: int
+        """
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children: return 0
+            cur = cur.children[char]
+        return cur.score
+
+
+class TrieNode:
+    __slots__ = 'children', 'score'
+
+    def __init__(self):
+        self.children = {}
+        self.score = 0
+
+    # Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
--- lintcode/91-minimum-adjustment-cost.py
+++ lintcode/91-minimum-adjustment-cost.py
@@ -8,15 +8,11 @@ class Solution:
     def MinAdjustmentCost(self, A, target):
         # write your code here
         if not A: return 0
-
-        import math
-        size = len(A)
         # dp[i][v]: the least cost of making A[i] = v
-        dp = [[0] * 101 for _ in range(size)]
-
-        for i in range(size):
+        dp = [[0] * 101 for _ in range(len(A))]
+        for i in range(len(A)):
             for j in range(1, 101):
-                dp[i][j] = math.inf
+                dp[i][j] = float('inf')
                 if i == 0:
                     dp[i][j] = abs(j - A[i])
                 else:
@@ -24,5 +20,4 @@ class Solution:
                         if abs(j - k) > target: continue
                         dif = abs(j - A[i]) + dp[i - 1][k]
                         dp[i][j] = min(dp[i][j], dif)
-
-        return min(dp[size - 1][i] for i in range(1, 101))
+        return min(dp[len(A) - 1][i] for i in range(1, 101))
