bbe47cb45437d58ac41ee93ca6d6f4ea1361bd36
leeshzh <leeshzh@outlook.com>
2018-12-17 22:01:29 +0800

    Linked List Cycle

---
+++ leetcode200/125-valid-palindrome.py
@@ -0,0 +1,37 @@
+"""
+125. Valid Palindrome
+Easy
+
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+"""
+
+
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if l < r and s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
