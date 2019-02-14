f774987275bba5529a6c370862fbe2f5a06b982f
GHLisz <leeshzh@outlook.com>
2019-02-14 21:31:32 +0800

    Find the Celebrity

---
+++ leetcode_tiq/Medium_Others/169-majority-element.py
@@ -0,0 +1,31 @@
+"""
+169. Majority Element
+Easy
+
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+"""
+
+
+class Solution:
+    def majorityElement(self, nums: 'List[int]') -> 'int':
+        major, count = None, 0
+        for n in nums:
+            if count == 0:
+                major, count = n, 1
+            elif major == n:
+                count += 1
+            else:
+                count -= 1
+        return major
---
+++ leetcode_tiq/Medium_Others/277-find-the-celebrity.py
@@ -0,0 +1,49 @@
+"""
+277. Find the Celebrity
+
+
+
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity.
+The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+Now you want to find out who the celebrity is or verify that there is not one.
+The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B.
+You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n),
+your function should minimize the number of calls to knows.
+
+Example
+Given n = 2
+
+2 // next n * (n - 1) lines
+0 knows 1
+1 does not know 0
+return 1 // 1 is celebrity
+
+Notice
+There will be exactly one celebrity if he/she is in the party.
+Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+"""
+
+"""
+The knows API is already defined for you.
+@param a, person a
+@param b, person b
+@return a boolean, whether a knows b
+you can call Celebrity.knows(a, b)
+"""
+
+
+class Solution:
+    # @param {int} n a party with n people
+    # @return {int} the celebrity's label or -1
+    def findCelebrity(self, n):
+        # Write your code here
+        knows = Celebrity.knows
+        res = 0
+        for i in range(n):
+            if knows(res, i):
+                res = i
+        for i in range(n):
+            if (res != i and knows(res, i)) or (not knows(i, res)):
+                return -1
+        return res
