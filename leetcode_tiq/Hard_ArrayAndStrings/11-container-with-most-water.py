e399b5223f03913cff979c1c01e6d89f33980879
leeshzh <leeshzh@outlook.com>
2019-07-03 21:26:23 +0800

    reformat problem descs

--- leetcode_tiq/Hard_ArrayAndStrings/11-container-with-most-water.py
+++ leetcode_tiq/Hard_ArrayAndStrings/11-container-with-most-water.py
@@ -7,10 +7,10 @@ Given n non-negative integers a1, a2, ..., an , where each represents a point at
 
 Note: You may not slant the container and n is at least 2.
 
+<p><img alt="" src="https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg" style="width: 600px; height: 287px;"></p>
 The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
 Example:
-
 Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 """
